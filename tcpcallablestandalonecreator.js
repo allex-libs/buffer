2c1212fa90dd595fbe8f57d5091079486adb3338
Luka.amazon <lukapetrovicsi@gmail.com>
2015-11-20 01:21:34 +0000

    Simple solution for sending OOB data

--- tcpcallablestandalonecreator.js
+++ tcpcallablestandalonecreator.js
@@ -3,9 +3,11 @@ var net = require('net');
 function createTcpCallableStandalone(execlib, bufferlib) {
   function Connection(callable, socket) {
     this.callable = callable;
+    this.callable.needToSend = this.sendData.bind(this);
     this.socket = socket;
     console.log('__methodDescriptors', this.callable.__methodDescriptors);
     this.rpcserver = new bufferlib.RPCLogicServer(this.callable, this.callable.__methodDescriptors, this.doSend.bind(this));
+    this.oobLogic = new bufferlib.Logic(['Char', 'String', 'Char', 'JSONString']);
     this.socket.on('data', this.onData.bind(this));
     this.socket.on('error', this.destroy.bind(this));
     this.socket.on('close', this.destroy.bind(this));
@@ -16,15 +18,24 @@ function createTcpCallableStandalone(execlib, bufferlib) {
     }
     this.rpcserver = null;
     this.socket = null;
+    this.callable.needToSend = null;
     this.callable = null;
   };
   Connection.prototype.onData = function (buffer) {
-    console.log('onData', buffer.toString());
+    //console.log('onData', buffer, buffer.toString());
     this.rpcserver.takeBuffer(buffer);
   };
   Connection.prototype.doSend = function (outbuff) {
     if (this.socket){
+      console.log('doSend', outbuff, outbuff.toString());
       this.socket.write(outbuff);
+    }
+  };
+  Connection.prototype.sendData = function (data) {
+    console.log('should send data', data);
+    var oob = data.oob;
+    if (oob) {
+      this.doSend(this.oobLogic.toBuffer(['o', oob[0], oob[1], oob[2]]));
     }
   };
 
